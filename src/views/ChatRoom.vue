c81bd0a0f197998e118a8a53d2eaf26521e3b2e5
Sciederrick <derrickmbarani@gmail.com>
2020-08-26 23:02:14 +0300

    please work!

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -25,7 +25,7 @@ export default{
   data(){
     return{
       username: "",
-      socket: io("http://comukol.herokuapp.com/chatroom"),
+      socket: io(),
       messages: [],
       users: []
     }
