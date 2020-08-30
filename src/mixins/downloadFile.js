8328ab25e5ab1b8dd72e6aa98aded50ee90221b6
Sciederrick <derrickmbarani@gmail.com>
2020-08-31 02:29:56 +0300

    fixed bugs

--- src/mixins/downloadFile.js
+++ src/mixins/downloadFile.js
@@ -2,21 +2,24 @@ import storage from '@/components/firebaseInit'
 export default{
   methods:{
     async downloadFile(e){
-      this.spinner=true
-      let el = e.target.textContent
-      let file = this.directoryPath.concat(el.trim())
-      const storageRef=storage.ref()
-      let fileRef=storageRef.child(file.trim())
-      console.log(file.trim())
-      try{
-        const url = await fileRef.getDownloadURL()
-        const iframe = document.getElementById('preview')
-        iframe.src = url
-        this.spinner=false
-      }catch(err){
-        window.alert(err.code)
-        console.log(err)
-        this.spinner=false
+      let download = confirm(`download ${e.target.textContent}?`)
+      if(download){
+        this.spinner=true
+        let el = e.target.textContent
+        let file = this.directoryPath.concat(el.trim())
+        const storageRef=storage.ref()
+        let fileRef=storageRef.child(file.trim())
+        console.log(file.trim())
+        try{
+          const url = await fileRef.getDownloadURL()
+          const iframe = document.getElementById('preview')
+          iframe.src = url
+          this.spinner=false
+        }catch(err){
+          window.alert(err.code)
+          console.log(err)
+          this.spinner=false
+        }
       }
     }
   }
