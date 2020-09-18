ccd296e97f1349511f1eccd701721a0a44ca0f74
Sciederrick <derrickmbarani@gmail.com>
2020-09-18 23:39:56 +0300

    resolving payload is undefined

--- server/index.js
+++ server/index.js
@@ -153,19 +153,19 @@ app.get('/api/resetpassword/:id/:token', (req, res)=>{
       const secret = `${user.password}-${Date.parse(user.updatedAt)}`
       try{
         const payload = jwt.verify(req.params.token, secret)
+        //Form to reset password
+        res.send(
+          '<form action="/api/resetpassword" method="POST">' +
+          '<input type="hidden" name="id" value="' + payload.id + '" />' +
+          '<input type="hidden" name="token" value="' + req.params.token + '" />' +
+          '<input type="password" name="password" value="" placeholder="Enter your new password..." />' +
+          '<input type="submit" value="Reset Password" />' +
+          '</form>'
+        )
       }catch(err){
         console.log(err)
         res.status(500).json({error:'invalid reset link'})
       }
-      //Form to reset password
-      res.send(
-      '<form action="/api/resetpassword" method="POST">' +
-      '<input type="hidden" name="id" value="' + payload.id + '" />' +
-      '<input type="hidden" name="token" value="' + req.params.token + '" />' +
-      '<input type="password" name="password" value="" placeholder="Enter your new password..." />' +
-      '<input type="submit" value="Reset Password" />' +
-      '</form>'
-    )
   })
 })
 
